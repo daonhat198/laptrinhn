b897105153b85ef72b78931aa87cb6796b451cec
daonhat198 <161843260+daonhat198@users.noreply.github.com>
2024-10-17 16:26:27 +0700

    up
    2

--- baitap17-10/App1/obj/Debug/net8.0-windows/App1.AssemblyInfo.cs
+++ baitap17-10/App1/obj/Debug/net8.0-windows/App1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f4a71257ff6ee21bb8e0e4adea065e6ffbf3c2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770d39f85ac536e76bda10b4fa37d4dd756e1986")]
 [assembly: System.Reflection.AssemblyProductAttribute("App1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
